09ac56ebf6c4820bc4dc4dd906ab0ce42e6f9961
Johnny Chang <johnny@johnnychang.com>
2016-04-02 11:20:32 -0500

    Started processing lyrics and implementing scrub and vectorize.

--- process_lyrics.py
+++ process_lyrics.py
@@ -1,7 +1,34 @@
 from nltk.corpus import stopwords
+from collections import defaultdict
 
 import os
 
-dirs_list = os.listdir("data")
-dirs = [d for d in dirs_list if d != ".DS_Store"]
+DATA_DIR = "data"
 
+def is_identifying(line):
+    return line.startswith("[") and line.endswith("]\r")
+
+def scrub_line(line):
+    ret = line.rstrip("\n")
+    print ret
+
+def scrub_lyrics(lyrics):
+    lyrics.remove("\n")
+    return [scrub_line(line) for line in lyrics]
+
+def vectorize_lyrics(lyrics):
+    scrubbed_lyrics = scrub_lyrics(lyrics)
+    return scrubbed_lyrics
+
+dirs_list = next(os.walk(DATA_DIR))[1]
+joined_dirs_list = [os.path.join(DATA_DIR, d) for d in dirs_list]
+
+for subdir in joined_dirs_list:
+    for root, dirs, files in os.walk(subdir):
+        for file_item in files:
+            file_path = os.path.join(subdir, file_item)
+
+            with open(file_path) as f:
+                lyrics = [line for line in f]
+
+            vectorize_lyrics(lyrics)
