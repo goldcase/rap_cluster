c0d12ed0c439e884f8acacc06aa02768caf854bc
Johnny Chang <johnny@johnnychang.com>
2016-04-02 14:42:21 -0500

    Added comments to a method and writing to the processed data directory.

--- process_lyrics.py
+++ process_lyrics.py
@@ -4,17 +4,29 @@ from collections import defaultdict
 import os
 
 DATA_DIR = "data"
+OUPUT_DATA_DIR = "processed_data"
 
 def is_identifying(line):
+    """
+    Determines whether the line passed into this method contains potentially
+    identifying information which may pollute clustering with artist information
+    encoded in a song.
+
+    Parameters
+    ----------
+    line : str
+        A line of a lyric. Identifying information generally is enclosed within
+        square brackets.
+    """
     return line.startswith("[") and line.endswith("]\r")
 
 def scrub_line(line):
     ret = line.rstrip("\n")
-    print ret
+    return ret
 
 def scrub_lyrics(lyrics):
     lyrics.remove("\n")
-    return [scrub_line(line) for line in lyrics]
+    return [scrub_line(line) for line in lyrics if not is_identifying(line)]
 
 def vectorize_lyrics(lyrics):
     scrubbed_lyrics = scrub_lyrics(lyrics)
@@ -22,6 +34,8 @@ def vectorize_lyrics(lyrics):
 
 dirs_list = next(os.walk(DATA_DIR))[1]
 joined_dirs_list = [os.path.join(DATA_DIR, d) for d in dirs_list]
+
+def write_lyrics_to_file(lyrics, artist):
 
 for subdir in joined_dirs_list:
     for root, dirs, files in os.walk(subdir):
@@ -31,4 +45,5 @@ for subdir in joined_dirs_list:
             with open(file_path) as f:
                 lyrics = [line for line in f]
 
-            vectorize_lyrics(lyrics)
+            vectorized_lyrics = vectorize_lyrics(lyrics)
+
