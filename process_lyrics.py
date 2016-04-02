4853e3b303121d64dc59229ca674d7aee96e6134
Johnny Chang <johnny@johnnychang.com>
2016-04-02 17:31:35 -0500

    Added regex to remove identifying information and method to write to file and check whether a directory exists. Cleaned up code to remove whitespace.

--- process_lyrics.py
+++ process_lyrics.py
@@ -1,10 +1,10 @@
 from nltk.corpus import stopwords
 from collections import defaultdict
 
-import os
+import os, re
 
 DATA_DIR = "data"
-OUPUT_DATA_DIR = "processed_data"
+OUTPUT_DATA_DIR = "processed_data"
 
 def is_identifying(line):
     """
@@ -18,24 +18,46 @@ def is_identifying(line):
         A line of a lyric. Identifying information generally is enclosed within
         square brackets.
     """
-    return line.startswith("[") and line.endswith("]\r")
+    return line.startswith("[") and line.endswith("]")
 
 def scrub_line(line):
-    ret = line.rstrip("\n")
+    ret = line.strip(" \t\r\n")
     return ret
 
 def scrub_lyrics(lyrics):
-    lyrics.remove("\n")
-    return [scrub_line(line) for line in lyrics if not is_identifying(line)]
+    ret = [line for line in lyrics]
+    ret = [re.sub("[\[]([\s\S]+?)[\]]", "", line) for line in lyrics]
+    ret = [scrub_line(line.lower()) for line in ret]
+    try:
+        lyrics.remove("\n")
+        lyrics.remove("\r\n")
+    except:
+        pass
+
+    ret = [line for line in ret if line != '']
+    return ret
 
 def vectorize_lyrics(lyrics):
     scrubbed_lyrics = scrub_lyrics(lyrics)
     return scrubbed_lyrics
 
+def write_lyrics_to_file(lyrics, song_name, artist):
+    if not os.path.isdir(OUTPUT_DATA_DIR):
+        os.mkdir(OUTPUT_DATA_DIR)
+
+    artist_path = os.path.join(OUTPUT_DATA_DIR, artist)
+
+    if not os.path.isdir(artist_path):
+        os.mkdir(artist_path)
+
+    lyric_path = os.path.join(artist_path, song_name)
+
+    with open(lyric_path, "w") as target:
+        str_lyrics = "\n".join(lyrics)
+        target.write(str_lyrics)
+
 dirs_list = next(os.walk(DATA_DIR))[1]
 joined_dirs_list = [os.path.join(DATA_DIR, d) for d in dirs_list]
-
-def write_lyrics_to_file(lyrics, artist):
 
 for subdir in joined_dirs_list:
     for root, dirs, files in os.walk(subdir):
@@ -45,5 +67,9 @@ for subdir in joined_dirs_list:
             with open(file_path) as f:
                 lyrics = [line for line in f]
 
-            vectorized_lyrics = vectorize_lyrics(lyrics)
+                vectorized_lyrics = vectorize_lyrics(lyrics)
+                _, artist = os.path.split(subdir)
+                write_lyrics_to_file(vectorized_lyrics,
+                                     os.path.basename(f.name),
+                                     artist)
 
